d627dd9b5d53bba40c2fabc70a70e73ad9bcb43a
akselkilliri-crypto <akselkilliri@gmail.com>
2026-03-16 23:15:14 +0300

    Update general.cpp

--- src/general.cpp
+++ src/general.cpp
@@ -3,11 +3,11 @@
 
 #ifdef LED
 void blink_led(int num_times, int blink_duration) {
-  for (int i = 0; i < num_times; i++) {
-    digitalWrite(LED, HIGH);
-    delay(blink_duration / 2);
-    digitalWrite(LED, LOW);
-    delay(blink_duration / 2);
-  }
+    for (int i = 0; i < num_times; i++) {
+        digitalWrite(LED, HIGH);
+        delay(blink_duration / 2);
+        digitalWrite(LED, LOW);
+        delay(blink_duration / 2);
+    }
 }
 #endif
